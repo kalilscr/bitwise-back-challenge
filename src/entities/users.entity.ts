9cdc2a3e6d0ca1b1772da7b10244945ef3b600c4
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-12 20:55:13 -0300

    feat: add users entity

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -7,23 +7,47 @@ export class UserEntity {
   id: bigint;
 
   @Property({ columnType: 'varchar', unique: true, length: 30 })
-  userName: string;
+  username: string;
 
-  @Property({ columnType: 'varchar', length: 30 })
+  @Property({
+    columnType: 'varchar',
+    length: 30,
+    nullable: true,
+  })
   name: string;
 
-  @Property({ columnType: 'varchar', length: 30 })
+  @Property({
+    columnType: 'varchar',
+    length: 30,
+    nullable: true,
+  })
   lastName: string;
 
   @Property({ columnType: 'varchar', nullable: true })
   profileImageUrl: string;
 
-  @Property({ columnType: 'varchar', nullable: true, length: 30 })
+  @Property({
+    columnType: 'varchar',
+    nullable: true,
+    length: 30,
+  })
   bio: string;
 
-  @Property({ columnType: 'varchar', unique: true })
+  @Property({
+    columnType: 'varchar',
+    unique: true,
+    nullable: true,
+  })
   email: string;
 
-  @Enum({ columnType: 'enum', items: () => Gender, nullable: true })
+  @Enum({
+    columnType: 'enum',
+    nativeEnumName: 'gender',
+    items: () => Gender,
+    length: 15,
+  })
   gender: Gender;
 }
+
+// when fetching from github data, name and email can be null only username should be obligatory
+// postgres automatically creates index to unique constraints and PK
